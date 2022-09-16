3138f9aa24a21a6a23bed84cef31a8f8c9ce555e
paavkar <62183220+paavkar@users.noreply.github.com>
2022-09-16 13:47:38 +0300

    The application now shows only the entries that the user has added themselves.

--- NET-Apex-Stats/ClientApp/src/index.tsx
+++ NET-Apex-Stats/ClientApp/src/index.tsx
@@ -1,11 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { reducer, StateProvider } from "./state";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
 <StateProvider reducer={reducer}>
